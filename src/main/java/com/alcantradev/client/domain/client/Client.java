dc0917655cded7dcc30d5e4421f1f8e5817f72ec
Vinícius de Alcântra Barros <vinicius.alcantra@somapay.br>
2024-05-12 23:46:20 -0300

    Adjusting details

--- src/main/java/com/alcantradev/client/domain/client/Client.java
+++ src/main/java/com/alcantradev/client/domain/client/Client.java
@@ -9,15 +9,15 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.alcantradev.client.infra.client.dto.input.ClientInputDTO;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- src/main/java/com/alcantradev/client/domain/client/PhoneClient.java
+++ src/main/java/com/alcantradev/client/domain/client/PhoneClient.java
@@ -7,13 +7,13 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.alcantradev.client.infra.client.dto.input.PhoneClientInputDTO;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -41,7 +41,7 @@ public class PhoneClient {
     @JoinColumn(name = "client_id")
     private Client client;
 
-    PhoneClient(PhoneClientInputDTO phoneClientInput){
+    public PhoneClient(PhoneClientInputDTO phoneClientInput){
         setDdd(phoneClientInput.getDdd());
         setNumber(phoneClientInput.getNumber());
     }
